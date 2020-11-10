53aee342a52f1c2c8a60e5bb84f2f1170de8de49
mowenxun <539996850@qq.com>
2020-11-10 11:49:57 +0800

    提交启动脚本

--- registy/start-code-dev.sh
+++ registy/start-code-dev.sh
@@ -4,10 +4,10 @@ cd $(dirname $0)
 img_mvn="maven:3.3.3-jdk-8"       # docker image of maven
 m2_cache=~/.m2                    # the local maven cache dir
 proj_home=$PWD                    # the project root dir
-img_output="xxxjob/com.xxl.job.admin" # output image tag
+img_output="xiaomo/registy" # output image tag
 
 apollo_configService="http://192.168.0.218:8080"
-app_id="deepexi.dd.middle.order"
+app_id="xiamo.registy"
 apollo_meta="http://192.168.0.218:8080"
 apollo_bootstrap_enabled="true"
 apollo_bootstrap_namespaces="application"
@@ -17,17 +17,17 @@ git pull # should use git clone https://name:pwd@xxx.git
 echo "use docker maven"
 docker run --rm \
   -v $m2_cache:/root/.m2 \
-  -v $proj_home:/usr/src/mymaven \
-  -w /usr/src/mymaven $img_mvn mvn clean package -U -Pdev -DskipTests
+  -v $proj_home:/usr/src/mymaven
+  #-w /usr/src/mymaven $img_mvn mvn clean package -U -Pdev -DskipTests
 
-mv $proj_home/target/xxl-job-admin-*.jar $proj_home/target/demo.jar # 兼容所有sh脚本
+mv $proj_home/deepexi-*.jar $proj_home/demo.jar # 兼容所有sh脚本
 docker build -t $img_output .
 
 mkdir -p $PWD/logs
 chmod 777 $PWD/logs
 
 # 删除容器
-docker rm -f com.xxl.job.admin &>/dev/null
+docker rm -f xiaomo/registy &>/dev/null
 
 version=$(date "+%Y%m%d%H")
 
@@ -36,7 +36,7 @@ docker run -d --restart=on-failure:5 --privileged=true \
   -w /home \
   --net=host \
   -v $PWD/logs:/home/logs \
-  --name com.xxl.job.admin xxxjob/com.xxl.job.admin \
+  --name xiaomo/registy \
   java \
   -Djava.security.egd=file:/dev/./urandom \
   -Duser.timezone=Asia/Shanghai \
