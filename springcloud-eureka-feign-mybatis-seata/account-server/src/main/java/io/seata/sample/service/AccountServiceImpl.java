d4e8a1f72f2be523f496f3357abea78001136dab
mowenxun <539996850@qq.com>
2020-11-24 17:06:43 +0800

    分布式事务

--- springcloud-eureka-feign-mybatis-seata/account-server/src/main/java/io/seata/sample/service/AccountServiceImpl.java
+++ springcloud-eureka-feign-mybatis-seata/account-server/src/main/java/io/seata/sample/service/AccountServiceImpl.java
@@ -2,7 +2,9 @@ package io.seata.sample.service;
 
 import io.seata.sample.dao.AccountDao;
 import io.seata.sample.feign.OrderApi;
+
 import java.math.BigDecimal;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +14,7 @@ import org.springframework.stereotype.Service;
  * @author IT云清
  */
 @Service("accountServiceImpl")
-public class AccountServiceImpl implements AccountService{
+public class AccountServiceImpl implements AccountService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AccountServiceImpl.class);
     @Autowired
@@ -22,24 +24,26 @@ public class AccountServiceImpl implements AccountService{
 
     /**
      * 扣减账户余额
+     *
      * @param userId 用户id
-     * @param money 金额
+     * @param money  金额
      */
     @Override
     public void decrease(Long userId, BigDecimal money) {
         LOGGER.info("------->扣减账户开始account中");
-        //模拟超时异常，全局事务回滚
-//        try {
-//            Thread.sleep(30*1000);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-        accountDao.decrease(userId,money);
+        //模拟异常
+        BigDecimal momey=new BigDecimal("abc123");
+       /* try {
+            Thread.sleep(30 * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+        accountDao.decrease(userId, money);
         LOGGER.info("------->扣减账户结束account中");
 
         //修改订单状态，此调用会导致调用成环
         LOGGER.info("修改订单状态开始");
-        String mes = orderApi.update(userId, money.multiply(new BigDecimal("0.09")),0);
-        LOGGER.info("修改订单状态结束：{}",mes);
+        String mes = orderApi.update(userId, money.multiply(new BigDecimal("0.09")), 0);
+        LOGGER.info("修改订单状态结束：{}", mes);
     }
 }
