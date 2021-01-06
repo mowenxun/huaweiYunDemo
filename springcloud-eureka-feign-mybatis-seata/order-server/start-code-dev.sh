c3f1b895f18a0829d7e40283ab42188c1ec52a5e
mowenxun <539996850@qq.com>
2021-01-06 11:51:14 +0800

    完善项目

--- springcloud-eureka-feign-mybatis-seata/order-server/start-code-dev.sh
+++ springcloud-eureka-feign-mybatis-seata/order-server/start-code-dev.sh
@@ -15,10 +15,11 @@ apollo_env="DEV"
 git pull # should use git clone https://name:pwd@xxx.git
 
 echo "use docker maven"
-docker run --rm \
-  -v $m2_cache:/root/.m2 \
-  -v $proj_home:/usr/src/mymaven \
-  -w $img_mvn mvn clean install -U -Pdev -DskipTests
+mvn clean package -U -Pdev -DskipTests
+#docker run --rm \
+#  -v $m2_cache:/root/.m2 \
+#  -v $proj_home:/usr/src/mymaven \
+#  -w $img_mvn mvn clean install -U -Pdev -DskipTests
   #-w /usr/src/mymaven $img_mvn mvn clean package -U -Pdev -DskipTests
 
 mv $proj_home/target/order-server-*.jar $proj_home/target/demo.jar # 兼容所有sh脚本
